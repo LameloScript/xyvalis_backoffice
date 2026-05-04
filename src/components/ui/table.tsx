01ca787a008832a95d96009ad8bd0ccbc91d1bc1
Daniel Boua <fd.boua@byte-ops.xyz>
2026-05-04 22:09:33 +0000

    feat: add shadcn UI component library (button, card, dialog, table, form fields, etc.)

---
+++ src/components/ui/table.tsx
@@ -0,0 +1,126 @@
+"use client"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Table({ className, ...props }: React.ComponentProps<"table">) {
+  return (
+    <div
+      data-slot="table-container"
+      className="relative w-full overflow-x-auto"
+    >
+      <table
+        data-slot="table"
+        className={cn("w-full caption-bottom text-sm", className)}
+        {...props}
+      />
+    </div>
+  )
+}
+
+function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
+  return (
+    <thead
+      data-slot="table-header"
+      className={cn(
+        "bg-primary",
+        "[&_tr]:!bg-primary [&_tr]:border-b-0 [&_tr]:hover:!bg-primary",
+        "[&_tr]:data-[state=selected]:!bg-primary",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TableBody({ className, ...props }: React.ComponentProps<"tbody">) {
+  return (
+    <tbody
+      data-slot="table-body"
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...props}
+    />
+  )
+}
+
+function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
+  return (
+    <tfoot
+      data-slot="table-footer"
+      className={cn(
+        "bg-muted/50 border-t font-medium [&>tr]:last:border-b-0",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
+  return (
+    <tr
+      data-slot="table-row"
+      className={cn(
+        "hover:bg-primary/5 data-[state=selected]:bg-primary/10 border-b border-border/60 transition-colors",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TableHead({ className, ...props }: React.ComponentProps<"th">) {
+  return (
+    <th
+      data-slot="table-head"
+      className={cn(
+        "!text-white h-11 px-4 text-left align-middle font-semibold text-xs uppercase tracking-wide whitespace-nowrap",
+        "[&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "[&_*]:!text-white",
+        "[&_button]:!font-semibold [&_button]:!uppercase [&_button]:!text-xs [&_button]:!tracking-wide [&_button]:!h-auto [&_button]:!px-0",
+        "[&_button]:!bg-transparent [&_button:hover]:!bg-transparent [&_button:focus]:!bg-transparent",
+        "[&>[role=checkbox]]:!border-white [&>[role=checkbox]]:!bg-white/10 [&>[role=checkbox]]:data-[state=checked]:!bg-white [&>[role=checkbox]]:data-[state=checked]:!text-primary",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TableCell({ className, ...props }: React.ComponentProps<"td">) {
+  return (
+    <td
+      data-slot="table-cell"
+      className={cn(
+        "p-4 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TableCaption({
+  className,
+  ...props
+}: React.ComponentProps<"caption">) {
+  return (
+    <caption
+      data-slot="table-caption"
+      className={cn("text-muted-foreground mt-4 text-sm", className)}
+      {...props}
+    />
+  )
+}
+
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+}
